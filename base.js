c6f5d606748ecb3673f3a8d121b254f6bcc853d5
Justin Reyes <Justin@Justins-MacBook-Pro.local>
2015-08-22 14:47:54 -0700

    Made movement a function that took arguments. Insead of having two seperate move functions

--- base.js
+++ base.js
@@ -86,45 +86,50 @@ function Enemy(position, img) {
 
 function movement(){
     $(window).on('keypress', function handleClick(e) {
-        function player2movement () {
-            //move right pressing l
-            if (e.which === p2Movement.right
-                && (player2.placement % player2Track.columns !== 0)) {
-                player2.placement++;
-                console.log(player2.placement);
+        function playerMovement (player, pmovement, track) {
+            //move right pressing the correct key
+            if (e.which === pmovement.right
+                && (player.placement % track.columns !== 0)) {
+                player.placement++;
+                console.log(player1.placement);
+                //console.log(player2.placement);
                 //addImg2(counter2);
                 //remImg2(counter2 - 1);
 
             }
-            //move left pressing j
-            if (e.which === p2Movement.left && (player2.placement !== 1)
-                && (player2.placement !== (1 + (player2Track.columns)))
-                && (player2.placement !== (1 + (player2Track.columns * 2)))
-                && (player2.placement !== (1 + (player2Track.columns * 3)))
-                && (player2.placement !== (1 + (player2Track.columns * 4)))) {
-                player2.placement--;
-                console.log(player2.placement);
+            //move left pressing the correct key
+            if (e.which === pmovement.left && (player.placement !== 1)
+                && (player.placement !== (1 + (track.columns)))
+                && (player.placement !== (1 + (track.columns * 2)))
+                && (player.placement !== (1 + (track.columns * 3)))
+                && (player.placement !== (1 + (track.columns * 4)))) {
+                player.placement--;
+                console.log(player1.placement);
+                //console.log(player2.placement);
                 //addImg2(counter2);
                 //remImg2(counter2 + 1);
             }
-            //move down pressing k
-            if (e.which === p2Movement.down && (player2.placement < player2Track.lastRow)) {
-                player2.placement += player2Track.columns;
-                console.log(player2.placement);
+            //move down pressing the correct key
+            if (e.which === pmovement.down && (player.placement < track.lastRow)) {
+                player.placement += track.columns;
+                console.log(player1.placement);
+                //console.log(player2.placement);
                 //addImg2(counter2);
                 //remImg2(counter2 - rows);
             }
-            //move up pressing i
-            if (e.which === p2Movement.up && (player2.placement !== 1)
-                && (player2.placement > player2Track.columns)) {
-                player2.placement -= player2Track.columns;
-                console.log(player2.placement);
+            //move up pressing the correct key
+            if (e.which === pmovement.up && (player.placement !== 1)
+                && (player.placement > track.columns)) {
+                player.placement -= track.columns;
+                console.log(player1.placement);
+                //console.log(player2.placement);
                 //addImg2(counter2);
                 //remImg2(counter2 + rows);
             }
         }
-        //calls and activates player 2 movement
-        player2movement();
+        //calls and activates player movements
+        playerMovement(player1, p1Movement, player1Track);
+        playerMovement(player2, p2Movement, player2Track);
         //finish line
         if ((player2.placement === player2Track.finishLine) && (play === 1)) {
             window.location.replace("player2Win.html");
