84d9223712af50258a298f4c24e436aa8aa2ced5
Justin Reyes <justin.regi.reyes@gmail.com>
2015-11-02 10:54:02 -0800

    added routes for '/', 'player1wins', and 'player2wins'

--- index.js
+++ index.js
@@ -6,12 +6,24 @@ var path = require('path');
 //port for server to listen to
 var port = process.env.PORT || 3000;
 
+//use a direct path to views
+var views = path.join(process.cwd(), '/views/');
+
 //serve js & css file
 app.use("/static", express.static("public"));
 
-app.get(['/'], function(req, res) {
+app.get('/', function(req, res) {
 	res.sendFile(process.cwd() + '/index.html');
 });
+
+app.get('/player1wins', function(req, res) {
+	res.sendFile(path.join(views, 'player1Win.html'));
+});
+
+app.get('/player2wins', function(req, res) {
+	res.sendFile(path.join(views, 'player2Win.html'));
+});
+
 
 app.listen(port, function() {
     console.log('Listening on port ' + port);
