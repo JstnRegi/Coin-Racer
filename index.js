b4c5ab64ef425b0632897f2fc38c1cd195d78dad
Justin Reyes <justin.regi.reyes@gmail.com>
2015-11-02 10:36:21 -0800

    created public folder. moved base.js andcs css there

---
+++ index.js
@@ -0,0 +1,18 @@
+//Requirements
+var express = require('express');
+var app = express();
+var path = require('path');
+
+//port for server to listen to
+var port = process.env.PORT || 3000;
+
+//serve js & css file
+app.use("/static", express.static("public"));
+
+app.get(['/'], function(req, res) {
+	res.sendFile(process.cwd() + '/index.html');
+});
+
+app.listen(port, function() {
+    console.log('Listening on port ' + port);
+});
